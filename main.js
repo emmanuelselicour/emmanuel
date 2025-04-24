45c3c2582c1e433a75831c6db35e6fa896812e8c
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2025-04-24 15:57:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// window.addEventListener("load", function () {
+//     setTimeout(function () {
+//       document.getElementById("loader").style.display = "none";
+//       document.getElementById("content").style.display = "block";
+//     }, 5000); // 5000ms = 5 seconds
+//   });
+
+
+
+window.addEventListener("load", function () {
+    setTimeout(function () {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("content").style.display = "block";
+    }, 5000);
+  });
+
